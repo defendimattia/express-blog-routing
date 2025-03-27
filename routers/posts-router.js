f383ed80ee9470ca1bdf40aef30579b5bdd452db
defendimattia <defendi.mattia92@gmail.com>
2025-03-27 14:44:21 +0100

    add: show route

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
+router.get("/", (req, res) => {
+    res.send("Lista dei post")
+})
 
+router.get("/:id", (req, res) => {
+    res.send(`Post numero ${req.params.id}`)
+})
 
 module.exports = router
